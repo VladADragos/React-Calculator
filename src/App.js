4dd1f4699cd1a6e8ca1f6d3b951593167bc02879
VladADragos <vlad6620@yahoo.com>
2018-12-07 12:35:40 +0100

    fixed keyboard validation

--- src/App.js
+++ src/App.js
@@ -20,75 +20,75 @@ class App extends Component {
       input === "-" ||
       input === "."
     ) {
-      console.log("Is Valid")
+      //console.log("Is Valid");
       return true;
     } else {
-      console.log("Is Not Valid")
+      //console.log("Is Not Valid");
       return false;
     }
   };
 
-  addToInput = e => {
+  write = e => {
     let value = e.target.value;
     if (this.isValidInput(value)) {
       if (value === "=") {
         this.setState({ input: eval(this.state.input) });
-      }else {
+      } else {
         this.setState({ input: this.state.input + value });
-        if(e !== undefined){
+        if (e !== undefined) {
           e.target.blur();
         }
       }
-    }  
+    }
   };
-  clear = (e) => {
+  clear = e => {
     this.setState({ input: "" });
-    
-    if(e !== undefined){
+
+    if (e !== undefined) {
       e.target.blur();
     }
   };
   enter = e => {
     try {
-      this.setState({ input: eval(this.state.input) });
+      if (this.state.input !== "") {
+        this.setState({ input: eval(this.state.input) });
+      }
+      if (eval(this.state.input) === Infinity) {
+        this.setState({ input: "Can't divide by 0" });
+        setTimeout(this.clear, 2000);
+      }
     } catch (err) {
+      console.log(err);
       this.setState({ input: "syntax error" });
       setTimeout(this.clear, 2000);
     }
-    if(e !== undefined){
+    if (e !== undefined) {
       e.target.blur();
     }
-    
   };
   change = event => {
-    //let input = e.target.keyCode;
-    console.log(event.target.value);
-    
-      
-      this.setState({ input: event.target.value });
-    
-      //this.setState({ input: e.target.value });
-    
-  };
-  gobal = e => {
-    if (e.keyCode === 13) {
-      e.target.value = "=";
-      this.addToInput(e);
+    let string = event.target.value;
+    let lastChar = string.substring(string.length - 1, string.length);
+    if (this.isValidInput(lastChar)) {
+      this.setState({ input: string });
     }
   };
-  handleKeyboardEnter=(event)=>{
-    
-    if(event.key==="Enter"){
-      this.enter(event);
-    } 
-  }
-  render() {
 
+  handleKeyboardEnter = event => {
+    if (event.key === "Enter") {
+      this.enter(event);
+    }
+  };
+  render() {
     return (
       <div className="calculator-container">
         <div className="calculator">
-          <Input input={this.state.input} onChange={this.change} handleKeyboardEnter={this.handleKeyboardEnter}/>
-          <Buttons handleClick={this.addToInput} handleEnter={this.enter} />
+          <Input
+            input={this.state.input}
+            onChange={this.change}
+            handleKeyboardEnter={this.handleKeyboardEnter}
+          />
+          <Buttons handleClick={this.write} handleEnter={this.enter} />
           <Clear handleClick={this.clear} />
         </div>
       </div>
